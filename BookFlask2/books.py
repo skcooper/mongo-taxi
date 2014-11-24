0505bef35f90c131ad260caf41641e961201d23d
James <jamestsweetman@gmail.com>
2014-11-24 14:09:21 -0600

    added locking table, two phase commiting hahah....

--- BookFlask2/books.py
+++ BookFlask2/books.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect, request
 import pymongo
 from bson.objectid import ObjectId
+import random
+import time
 
 app = Flask(__name__, static_url_path = "")
 connection_string = "mongodb://127.0.0.1"
@@ -8,10 +10,14 @@ connection = pymongo.MongoClient(connection_string)
 database = connection.taxi
 rides = database.rides
 driver = database.driver
+# used to keep track of the rides being claimed by the driver
+locks = database.locks
 
 #Add first (and so far, only) driver
 if driver.find({'name':'driver1'}).count() == 0:
 	driver.insert({'name':'driver1', 'car':'car1', 'phone':'phone1', 'rate':'rate1'})
+if driver.find({'name':'driver2'}).count() == 0:
+	driver.insert({'name':'driver2', 'car':'car2', 'phone':'phone2', 'rate':'rate2'})
 
 #Homepage
 @app.route('/')
@@ -42,15 +48,16 @@ def detail(ride_id):
 	return render_template('detail.html', result=results)
 
 
-@app.route('/claim/<name>/', methods=['GET', 'POST'])
-def claim(name):	
-	cursor = rides.find_one({'name':name})
+@app.route('/claim/<ride_id>/', methods=['GET', 'POST'])
+def claim(ride_id):	
+	cursor = rides.find_one({'_id': ObjectId(ride_id)})
 	cursor_dict = {field:value for field, value in cursor.items()}
+	trans_dict = {}
 	already_claimed = cursor_dict['claimed']
 	if request.method == 'GET':
 		claimed_result = {field:value for field, value in cursor.items()}
 		claimed_result['claimed'] = True
-		rides.update({'name':name}, claimed_result)
+		rides.update({'_id':ObjectId(ride_id)}, claimed_result)
 
 	elif request.method == 'POST':
 		# new_data.update({field:value for field, value in cursor.items()})
@@ -58,17 +65,31 @@ def claim(name):
 		cur_driver = driver.find_one({'name':'driver1'})
 		new_driver_data = {field:value for field,value in cur_driver.items()}
 		#Add driver name to ride
-		cursor_dict.update({'driver_name':new_driver_data['name']})
-		rides.update({'name':name}, cursor_dict)
+		if cursor_dict['transaction'] == 'initial' and locks.find_and_modify(
+			query= {'_id': ObjectId(ride_id)},
+			update= {"$set": {'open': False, 'driver': cur_driver}},
+			full_response= True
+		):
+			cursor_dict.update({'transaction':'pending'})
+			cursor_dict.update({'driver_name':new_driver_data['name']})
+			cursor_dict.update({'transaction':'finished'})
+			rides.update({'_id':ObjectId(ride_id)}, cursor_dict)
+
+		if cursor_dict['transaction'] == 'pending':
+			cursor_dict.update({'transaction':'cancelled'})
+			cursor_dict.update({'driver_name':''})
+			rides.update({'_id':ObjectId(ride_id)}, cursor_dict)
+
 		#Add ride _id to driver list
-		if 'rides' not in new_driver_data :
+		if 'rides' not in new_driver_data:
 			new_driver_data['rides'] = {}
 		new_driver_data['rides'].update({str(cursor_dict['_id']):cursor_dict['name']})
 
 		driver.update({'_id':new_driver_data['_id']}, new_driver_data)
 
-		cursor = rides.find_one({'name':name})
-			
+		cursor = rides.find_one({'_id':ObjectId(ride_id)})
+
+		locks.remove({'_id': ObjectId(ride_id)})
 	results = {field:value for field, value in cursor.items() if field != 'claimed'}
 	return render_template('claim.html', result = results, already_claimed = already_claimed)
 
@@ -134,6 +155,18 @@ def add():
 	if request.method == 'POST':
 		new_data = {k : v for k, v in request.form.items()}
 		new_data['claimed'] = False
+		new_data['transaction'] = 'initial'
+
+		# trans_ride = rides.find_one({'name':name})
+		# trans_ride_data = {field:value for field,value in trans_ride.items()}
+		# transaction_data = {"ride_id": trans_ride_data['_id'], "driver_id": new_driver_data['_id'], "lock": True}
+		# transactions.insert(transaction_data)
+		# transaction_data = {"ride_id" : rides.find_one({'name':name})}
+		# transactions.insert(transaction_data)
+
+		# trans_dict = convert_to_dict(transactions.find_one())
+		# print trans_dict
+
 		#If the user leaves a field blank
 		if new_data['name'] == '' or new_data['phone'] == '' or new_data['pickup'] == '' or new_data['destination'] == '' or new_data['time'] == '':
 			return render_template('add.html', alert="required")
@@ -141,7 +174,12 @@ def add():
 		elif rides.find({'name':new_data['name'], 'time':new_data['time']}).count() > 0: 
 			return render_template('add.html', alert="exists")
 		else:
-			rides.insert(new_data);
+			rides.insert(new_data)
+			newest_ride = rides.find_one({'name': new_data['name'], 'time':new_data['time'], 'pickup':new_data['pickup'],
+				'destination':new_data['destination'], 'phone':new_data['phone']})
+			newest_dict = {field:value for field, value in newest_ride.items()}
+			locks.insert({'ride_id': newest_dict['_id'], 'timestamp': time.time(), 'open': True, 'driver_id': ''})
+			print convert_to_dict(locks.find())
 			return render_template('add.html', alert = "success")
 	else:
 		return render_template('add.html', alert="")
